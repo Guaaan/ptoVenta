7a88eca29c8a0ec630294eb9e9bfdb77bdef9dc4
Guaaan <Juampablo.txt@gmail.com>
2021-06-16 10:58:46 -0400

    campos numericos creación de clases

---
+++ formatoNumerico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ptoVenta
+{
+    public class formatoNumerico
+    {
+        public void FormatoMoneda(TextBox xTBox)
+        {
+            if (xTBox.Text == string.Empty)
+            {
+                return;
+            }
+            else
+            {
+                decimal Monto;
+                Monto = Convert.ToDecimal(xTBox.Text);
+                xTBox.Text = Monto.ToString("N2");
+            }
+        }
+    }
+}
